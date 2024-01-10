9c395e54dc5c5bb7dc486070cf2dd2f6ddfa83f7
Himanshu Bhushan <himanshubhushan21@gmail.com>
2024-01-10 20:34:10 +0530

    program for macos

---
+++ weatherapp.py
@@ -0,0 +1,12 @@
+#weatherapp
+import requests
+import json
+import os
+city = input("Enter the city name : \n")
+url = f'http://api.weatherapi.com/v1/current.json?key=c798733495ea461ead0135236240801&q={city}'
+r =requests.get(url)
+#print(r.text)
+wdic = json.loads(r.text)
+w = wdic['current']['temp_c']
+print(w)
+os.system(f'say temperature in {city} is {w} degree celcius')
